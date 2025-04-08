b569effca175655ef48d7e73f8b1193ddcf999af
liubo20190515 <liubo20190515@gmail.com>
2025-04-08 19:33:54 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,4 @@
 import { handle } from "hono/vercel"; // Use the generic Vercel adapter for Edge
-import app from "../main.js"; // <-- Reverted path
+import app from "../dist/main.js"; // <-- Point back to compiled JS in dist
 
 export default handle(app);
-
